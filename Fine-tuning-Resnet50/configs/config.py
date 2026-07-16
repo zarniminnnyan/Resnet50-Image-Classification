e873c971d53ca5c7bd2cd1a1397b6ab2515858e0
Zarni Minn Nyan <zarniminnnyan@gmail.com>
2026-07-17 00:52:18 +0700

    Write the baseline results to json file in results/ dir

--- Fine-tuning-Resnet50/configs/config.py
+++ Fine-tuning-Resnet50/configs/config.py
@@ -1,8 +1,13 @@
 import torch
+import warnings 
 
 # Device setup
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+#Set warning if CUDA is not available 
+if not torch.cuda.is_available():
+    warnings.warn("CUDA is not available. Running on CPU will be significantly slower.", UserWarning)
+    
 # Dataset settings
 ROOT = "./root"
 BATCH_SIZE = 32
--- Fine-tuning-Resnet50/scripts/evaluate_baseline.py
+++ Fine-tuning-Resnet50/scripts/evaluate_baseline.py
@@ -1,5 +1,7 @@
 import torch  
 import tqdm
+import json
+import os 
 from  model import  load_resnet
 from data import load_tv_dataset
 from utils import get_model_size,measure_inference_time
@@ -10,7 +12,7 @@ def evaluate_baseline_model(model,eval_loader,device):
     correct = 0
     total = 0
     model.eval()
-    tqdm_eval_loader=tqdm(eval_loader,leave=False,desc="Evaluating the pretrained Resnet50 baseline")
+    tqdm_eval_loader=tqdm.tqdm(eval_loader,leave=False,desc="Evaluating the pretrained Resnet50 baseline")
     
     for img,label  in tqdm_eval_loader: 
         img,label=img.to(device),label.to(device)
@@ -33,11 +35,28 @@ def main():
     inference_time=measure_inference_time(resnet50,DEVICE)
     model_accuracy=evaluate_baseline_model(resnet50,val_loader,DEVICE)
     
-    print(f"\nResNet50 Baseline Results:")
-    print("-"*35)
-    print(f"  • Model Size       : {model_size}")
-    print(f"  • Inference Time   : {inference_time}")
-    print(f"  • Accuracy         : {model_accuracy:.2f}%")
+    baseline_results = {
+    "Model Size (MB)": round(model_size,2),
+    "Inference Time (ms)": round(inference_time,2),
+    "Accuracy (%)": round(model_accuracy,2)
+    }
+    
+    # Get the absolute path to the results directory
+    results_dir = os.path.join(os.path.dirname(__file__), "..", "results")
+    os.makedirs(results_dir, exist_ok=True)
+
+    # Full path to the JSON file
+    json_path = os.path.join(results_dir, "baseline_results.json")
+    #Write the results to json file
+    with open(json_path,"w") as f:
+        json.dump(baseline_results,f,indent=4)
+        
+    #Print the dict baseline results
+    print("\n"*2)
+    print("ResNet50 Baseline Results:")
+    print("\n"*2)
+    print(baseline_results)
+    
 
 if __name__=="__main__":
     main()
