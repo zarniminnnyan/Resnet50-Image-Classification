073c6f7281d5997cbaebed9fe25d246e5fac5ee1
Zarni Minn Nyan <zarniminnnyan@gmail.com>
2026-07-22 20:45:47 +0700

    Modified the config.py for kaggle

--- Fine-tuning-Resnet50/configs/config.py
+++ Fine-tuning-Resnet50/configs/config.py
@@ -17,17 +17,16 @@ BATCH_SIZE = 64
 SPLIT_SIZE = 0.8
 SEED = 42
 
-ROOT = "/content/drive/MyDrive/dataset/cifar-100-python"
-WEIGHT_PATH = "./models"
-
-#Model Checkpoint Dir 
-LIGHTNING_LOGS_PATH="/content/drive/MyDrive/lightning_logs" 
-MODEL_CHECKPOINT_PATH="/content/drive/MyDrive/checkpoints"
+# Kaggle paths
+ROOT = "/kaggle/input/cifar-100-python"       
+WEIGHT_PATH = "/kaggle/working/models"       
+LIGHTNING_LOGS_PATH = "/kaggle/working/logs
+MODEL_CHECKPOINT_PATH = "/kaggle/working/checkpoints"
 
 # Flags
 INFERENCE = False
 LR=1e-4
-NUM_EPOCHS=20
+NUM_EPOCHS=20 
 
 OPTUNA_EPOCHS = 10
 
